cdb1daf9608d808abcfb944657e77e697af344bf
Ryan Choe <ryan.choe@outlook.com>
2020-11-23 21:26:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,9 +13,9 @@ end
 
 def selection(num)
   if num = 1
-    "YUM YUM MUNCH MUNCH MUNCH"
+    1 = "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-    "HAM HAM HAM IN MY TUMMY"
+    2 = "HAM HAM HAM IN MY TUMMY"
   end
 end
 
